a8f72bd0ab3cb55785290abc3d2bb3f57bf0ff44
Ahmed <ahmeddhalilovic@gmail.com>
2024-09-04 23:56:56 +0200

    Initial Commit

---
+++ NewsAppTwo/NewsAppTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTwoApp.swift
+//  NewsAppTwo
+//
+//  Created by Ahmed Halilovic on 4. 9. 2024..
+//
+
+import SwiftUI
+
+@main
+struct NewsAppTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
